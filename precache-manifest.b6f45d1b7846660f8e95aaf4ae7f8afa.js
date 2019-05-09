1e5aea4342e4ad246e926b7d3ca1b3f0d7d9ae68
Tatiana Pyatykhina <tpyatyhina@mail.ru>
2019-05-09 23:59:00 +0300

    Updates

---
+++ precache-manifest.b6f45d1b7846660f8e95aaf4ae7f8afa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053d9c8e7c130b20d47788e6108afb5a",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "636880c70d0835c73fda",
+    "url": "/kanban/static/css/main.00a6cdad.chunk.css"
+  },
+  {
+    "revision": "b9c7c3540b813344802a",
+    "url": "/kanban/static/js/2.da328fc9.chunk.js"
+  },
+  {
+    "revision": "636880c70d0835c73fda",
+    "url": "/kanban/static/js/main.1be47c77.chunk.js"
+  },
+  {
+    "revision": "9c81d15565ccb12a85c0",
+    "url": "/kanban/static/js/runtime~main.4f25e9d8.js"
+  },
+  {
+    "revision": "6cb6e5fc57e9600d8f9f7cdfaa120d3d",
+    "url": "/kanban/static/media/add.6cb6e5fc.svg"
+  },
+  {
+    "revision": "0adf851bce82cc59f5438658221e0a6b",
+    "url": "/kanban/static/media/background.0adf851b.png"
+  },
+  {
+    "revision": "5fe0db661a8f134c8ff7cb418373c469",
+    "url": "/kanban/static/media/cross.5fe0db66.svg"
+  }
+]);
